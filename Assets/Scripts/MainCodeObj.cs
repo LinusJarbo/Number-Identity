b2c52ea1a36043a0820a6964aa066e91261eab96
Linus Jarbo <linus@linusjarbo.com>
2020-08-13 10:48:10 +0200

    initial clean
    Added new TODOs, cleaned up code

--- Assets/Scripts/MainCodeObj.cs
+++ Assets/Scripts/MainCodeObj.cs
@@ -1,11 +1,9 @@
-﻿
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 
 using UnityEngine;
 using UnityEngine.UI;
-
 
 public class MainCodeObj : MonoBehaviour
 {
@@ -15,31 +13,22 @@ public class MainCodeObj : MonoBehaviour
 
     public Toggle toggle;
 
-
     /* TODO 
-    GET TEXT IN INPUT FIELD 
-    validate input filed
-    set text as two arrays
-    reverse one of the arrays
-    mix the arrays in step for as long as the 
-
-    upload to homepage
+    Remove last digit in identity number
+    check if copy to memory works in WebGL?
+    checkbox readd first digit to identitynumber
     */
 
     private void Start()
     {
         textInput.text = "1234";
-        //textInput.text = "add numberseq here...";
     }
 
     //called when button pressed
     public void CalculateFoldIdentity()
     {
-        //Debug.Log("CalculateFoldIdentity Called");
         //get text
         inputText = GetInputFieldText();
-
-
 
         //text ok, clear output
         clearOutputText();
@@ -51,6 +40,7 @@ public class MainCodeObj : MonoBehaviour
         {
             //cut up string in two halfs and fold
             string s = lines[j];
+
             //validate string
             s = validateText(s);
 
@@ -58,12 +48,11 @@ public class MainCodeObj : MonoBehaviour
             int strLength = s.Length; //need length twice
             for (int i = 0; i < strLength; i++)
             {
-                
-
                 if (i == 0)
                 {
                     addTextToOutput("\n" + "this is input: " + s);
                 }
+
                 if (toggle.isOn)
                 {
                     addTextToOutput(s);
@@ -96,9 +85,6 @@ public class MainCodeObj : MonoBehaviour
         //reverse second part
         secondPart = Reverse(secondPart);
 
-        //Debug.Log(firstPart);
-        //Debug.Log(secondPart);
-
         char[] firstPartCharArr = firstPart.ToCharArray();
         char[] secondPartCharArr = secondPart.ToCharArray();
 
@@ -108,19 +94,15 @@ public class MainCodeObj : MonoBehaviour
             mixedString += firstPartCharArr[i];
             mixedString += secondPartCharArr[i];
         }
-
-        //addTextToOutput(mixedString);
-
         return mixedString;
-
-        //Debug.Log(mixedString);
     }
 
-    //save text to file
-    public void copyToMemory(){
+    public void copyToMemory()
+    {
         Debug.Log("Button save pressed");
         textOutput.text.CopyToClipboard();
     }
+
     private string GetInputFieldText()
     {
         //Debug.Log(textInput.text);
@@ -132,16 +114,14 @@ public class MainCodeObj : MonoBehaviour
         if (s.Length % 2 == 0)
         {
             //do nothing
-            //Debug.Log("length ok");
         }
         else
         {
-            //Debug.Log("length not ok, trimming");
             addTextToOutput("Size not even, trimming end from: " + s);
-            s = s.Remove(s.Length -1);
+            s = s.Remove(s.Length - 1);
             addTextToOutput("->                            to: " + s);
         }
-        return(s);
+        return (s);
     }
 
     private void addTextToOutput(string s)
